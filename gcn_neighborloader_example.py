e4138f5cf3f5a191da0305657d93198d4c631b66
Rishi Puri <puririshi98@berkeley.edu>
2023-10-11 14:44:31 -0700

    Update gcn_neighborloader_example.py

--- gcn_neighborloader_example.py
+++ gcn_neighborloader_example.py
@@ -59,7 +59,6 @@ benchmark_epoch_time(openAI_gcn)
 print("compiling GCN w/ NVIDIA libs...")
 torch._dynamo.reset()
 import lightning
-print("Timing compile w/ nvfuser:")
 nvfuser_gcn = lightning.compile(gcn, dynamic=True)
 print("Timing compile w/ nvfuser:")
 benchmark_epoch_time(nvfuser_gcn)
